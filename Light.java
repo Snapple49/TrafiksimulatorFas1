2760ed6a98787eccbad895b08223be47f7b2fb7f
Nallexander <alexander.e.lind@gmail.com>
2014-10-29 14:35:21 +0100

    fixade Light och Car

--- Light.java
+++ Light.java
@@ -1,19 +1,39 @@
+
+
 public class Light {
     private int period;
     private int time;  // Intern klocka: 0, 1, ... period-1, 0, 1 ...
     private int green; // Signalen grön när time<green 
 
-    public Light(int period, int green) {...}
+    public Light(int period, int green) {
+	this.period = period;
+	this.green = green;
+    }
 
-    public void    step() { 
+    public void    step() {
+	this.time++;
        // Stegar fram klocka ett steg
     }
 
     public boolean isGreen()   {
+	return (this.time < this.green);
 	// Returnerar true om time<green, annars false
     }
 
+    
+
     public String toString() {
 	return "Light(period=" + this.period + ", time=" + this.time + ", green=" + this.green + ")";
-    	
+    }
+
+    public static void main(String [] args) {
+	Light x = new Light(5, 5);
+	Light y = new Light(1, 1);
+	System.out.println(x.toString() + "\n" + y.toString() + "\n" + y.isGreen());
+	y.step();
+	System.out.println(y.isGreen());
+	
+    }
+	
+a
 }
