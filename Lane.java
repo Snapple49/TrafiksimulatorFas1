0d57c241fa00bbeb458e1bbeec86d19b11dc95d3
Nallexander <alexander.e.lind@gmail.com>
2014-10-30 13:45:17 +0100

    Skrev klart Lane

--- Lane.java
+++ Lane.java
@@ -17,6 +17,11 @@ public class Lane {
 	// Stega fram alla fordon (utom det på plats 0) ett steg 
         // (om det g�r). (Fordonet på plats 0 tas bort utifrån 
 	// mm h a metoden nedan.)
+	Car firstcar = this.getFirst();
+	for (int i = 1; i < theLane.length; i++) {
+	    theLane[i-1] = theLane[i]; 
+	}
+	
     }
 
     public Car getFirst() {
@@ -28,20 +33,38 @@ public class Lane {
 
     public Car firstCar() {
 	// Returnera bilen som står först utan att ta bort den
+	return theLane[0];
     }
 
 
     public boolean lastFree() {
 	// Returnera true om sista platsen ledig, annars false
+	return (theLane[theLane.length - 1] == null);
     }
 
     public void putLast(Car c) throws OverflowException {
 	// Ställ en bil på sista platsen på vägen
 	// (om det går).
+	if (theLane[theLane.length - 1] == null) {
+	    theLane[theLane.length - 1] = c;
+	}
+	else {
+	    throw new OverflowException;
+	}
     }
 
     
     public String toString() {
 	return "Lane(theLane=" + this.theLane + ")";
+    }
+
+    public static void main(String [] args) {
+	Lane newLane = Lane(10);
+	Car car1 = Car(5, 5);
+	newLane.putLast(car1);
+	newLane.step();
+	Car car2 = Car(4, 4);
+	newLane.putLast(car2);
+	newLane.step();
     }
 }
