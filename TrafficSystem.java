2cfb759bc93fbf25a16feaf769a9aa73085ce09e
Nallexander <alexander.e.lind@gmail.com>
2014-11-07 11:32:05 +0100

    Flyttade enterValues() till TrafficSystem

--- TrafficSystem.java
+++ TrafficSystem.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 
 
@@ -36,16 +38,139 @@ public class TrafficSystem {
     	intensity = 1;
     	leftIntensity = 3;
     }
-    
 
-    public void readParameters() {
+public void enterValues() {
+	int intensity = 0;
+	int leftIntensity = 0;
+	int leftIntensityNumerical = 0;
+	int period = 0;
+	int greenPeriodStraight = 0;
+	int greenPeriodTurn = 0;
+	int r0 = 0;
+	int r1 = 0;
+	
+	Scanner sc = new Scanner(System.in);
+
+	do{
+		try {
+
+			System.out.println("Please enter intensity (must be between 0 and 11). \n");
+			intensity = sc.nextInt();
+		}
+		catch (java.util.InputMismatchException e) {
+		System.out.println("Not a number!");
+		sc.nextLine();
+		intensity = 0;
+		}
+	} while (intensity <= 0 || intensity > 10);
+
+	do{
+		try {
+			sc.nextLine();
+			System.out.println("Please enter leftIntensity.\n");
+			leftIntensity = sc.nextInt();
+			leftIntensityNumerical = 1;
+		}
+		catch (java.util.InputMismatchException e) {
+		System.out.println("Not a number!");
+		leftIntensity = 0;
+		leftIntensityNumerical = 0;
+		}
+	} while (leftIntensityNumerical == 0);
+	
+	do{
+		try {
+
+			System.out.println("Please enter period (must be larger than 1). \n");
+			period = sc.nextInt();
+		}
+		catch (java.util.InputMismatchException e) {
+		System.out.println("Not a number!");
+		sc.nextLine();
+		period = 0;
+		}
+	} while (period <= 1);
+	
+	do{
+		try {
+
+			System.out.println("Please enter green period for straight (must be larger than 0 and smaller than period). \n");
+			greenPeriodStraight = sc.nextInt();
+		}
+		catch (java.util.InputMismatchException e) {
+		System.out.println("Not a number!");
+		sc.nextLine();
+		greenPeriodStraight = 0;
+		}
+	} while (greenPeriodStraight <= 0 || greenPeriodStraight >= period);
+	
+	do{
+		try {
+
+			System.out.println("Please enter green period for turning (must be larger than 0 and smaller than period). \n");
+			greenPeriodTurn = sc.nextInt();
+		}
+		catch (java.util.InputMismatchException e) {
+		System.out.println("Not a number!");
+		sc.nextLine();
+		greenPeriodTurn = 0;
+		}
+	} while (greenPeriodTurn <= 0 || greenPeriodTurn >= period);
+	
+	do{
+		try {
+
+			System.out.println("Please enter length of first part of lane (must be larger than 0). \n");
+			r0 = sc.nextInt();
+		}
+		catch (java.util.InputMismatchException e) {
+		System.out.println("Not a number!");
+		sc.nextLine();
+		r0 = 0;
+		}
+	} while (r0 <= 0);
+	
+	do{
+		try {
+
+			System.out.println("Please enter length of second part of lane (must be larger than 0). \n");
+			r1 = sc.nextInt();
+		}
+		catch (java.util.InputMismatchException e) {
+		System.out.println("Not a number!");
+		sc.nextLine();
+		r1 = 0;
+		}
+	} while (r1 <= 0);
+	
+
+
+	sc.close();
+	this.intensity = intensity;
+	this.leftIntensity = leftIntensity;
+	this.s1 = new Light(period, greenPeriodStraight);
+	this.s2 = new Light(period, greenPeriodTurn);
+	this.r0 = new Lane(r0);
+	this.r1 = new Lane(r1);
+	this.r2 = new Lane(r1);
+	
+}
+
+    public void readParameters(int input) {
 	// Läser in parametrar för simuleringen
 	// Metoden kan läsa från terminalfönster, dialogrutor
 	// eller från en parameterfil. Det sista alternativet
 	// är att föredra vid uttestning av programmet eftersom
 	// man inte då behöver mata in värdena vid varje körning.
     // Standardklassen Properties är användbar för detta. 
-    	
+    	switch (input) {
+    	case 1:
+    		this.enterValues(); 
+    		break;
+    	case 2:
+    		this.getProperties();
+    		break;
+    	}
     }
 
     public TrafficSystem(int intensity, int leftIntensity, int period, int greenPeriodStraight, int greenPeriodTurn, int r0, int r1){
