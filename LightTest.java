0edcedc9f60e1725d15b348ea504495472a18938
Nallexander <alexander.e.lind@gmail.com>
2014-11-11 21:10:04 +0100

    100% LightTest

--- LightTest.java
+++ LightTest.java
@@ -13,20 +13,34 @@ public class LightTest {
 	
 	@Test
 	public void testStep() {
-		Light tester = new Light(5,5);
+		Light tester = new Light(150,120);
 		tester.step();
 		assertEquals(1, tester.getTime());
 		for (int i = 0; i < 99; i++ ) {
 			tester.step();
 		}
+		
 		assertEquals(100, tester.getTime());
 	}
 	@Test
+	public void testPeriodStep() {
+		Light tester = new Light(2,1);
+		tester.step();
+		tester.step();
+		assertEquals(0, tester.getTime());
+		
+	}
+	@Test
 	public void testIsGreen() {
-		Light tester = new Light(1, 1); 
+		Light tester = new Light(2, 1); 
 		assertEquals("isGreen = True", true, tester.isGreen());
 		tester.step();
 		assertEquals("isGreen = False", false, tester.isGreen());
+	}
+	@Test
+	public void testToString() {
+		Light tester = new Light(1,1);
+		assertEquals("Light(period=1, time=0, green=1)", tester.toString());
 	}
 
 }
