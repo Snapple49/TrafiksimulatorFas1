604c92fa3a144ed6b19f7185854738cd788a4f28
Nallexander <alexander.e.lind@gmail.com>
2014-11-12 14:10:59 +0100

    La till lite Javadoc

--- Lane.java
+++ Lane.java
@@ -1,21 +1,42 @@
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Lane.
+ */
 public class Lane {
 
+    /**
+     * The Class OverflowException.
+     */
     public static class OverflowException extends RuntimeException {
         // Undantag som kastas nar det inte gick att lagga 
         // in en ny bil pa vagen
-    	public OverflowException(String errormsg){
+    	/**
+         * Instantiates a new overflow exception.
+         *
+         * @param errormsg the errormsg
+         */
+        public OverflowException(String errormsg){
     		System.err.println(errormsg);
     	}
     }
 
+    /** The lane. */
     private Car[] theLane;
 
+    /**
+     * Instantiates a new lane.
+     *
+     * @param n the n
+     */
     public Lane(int n) {
     	// Konstruerar ett Lane-objekt med plats for n fordon
     	this.theLane = new Car[n];
     }
 
+    /**
+     * Step.
+     */
     public void step() {
 	// Stega fram alla fordon (utom det på plats 0) ett steg 
         // (om det gar). (Fordonet på plats 0 tas bort utifrån 
@@ -27,6 +48,12 @@ public class Lane {
 		theLane[theLane.length-1] = null;
 		
     }
+	
+	/**
+	 * Step.
+	 *
+	 * @param green the green
+	 */
 	public void step(boolean green) {
 		if (green) {
 			this.step();
@@ -40,27 +67,50 @@ public class Lane {
 			}	
 		}
 	}
-    
-    public Car getFirst() {
+   
+	public Car getFirst() {
 	// Returnera och tag bort bilen som står först
     	Car temp = this.theLane[0];
     	this.theLane[0] = null;
     	return temp;
     }
+    
+    /**
+     * First car.
+     *
+     * @return the car
+     */
     public Car firstCar() {
 	// Returnera bilen som står först utan att ta bort den
 	return theLane[0];
     }
 
+    /**
+     * Pos free.
+     *
+     * @param pos the pos
+     * @return true, if successful
+     */
     public boolean posFree(int pos) {
     	return (theLane[pos] == null);
     }
 
+    /**
+     * Last free.
+     *
+     * @return true, if successful
+     */
     public boolean lastFree() {
 	// Returnera true om sista platsen ledig, annars false
 	return (theLane[theLane.length - 1] == null);
     }
 
+    /**
+     * Put last.
+     *
+     * @param c the c
+     * @throws OverflowException the overflow exception
+     */
     public void putLast(Car c) throws OverflowException {
 	// Stall en bil pa sista platsen pa vagen
 	// (om det gar).    	
@@ -71,6 +121,9 @@ public class Lane {
     }
 
     
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     public String toString() {
     	String returnString = "Lane:\n";
     	for (int i = 0; i < theLane.length; i++) {
--- Light.java
+++ Light.java
@@ -1,20 +1,44 @@
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Light.
+ */
 public class Light {
+    
+    /** The period. */
     private int period;
+    
+    /** The time. */
     private int time;  // Intern klocka: 0, 1, ... period-1, 0, 1 ...
+    
+    /** The green. */
     private int green; // Signalen grön när time<green 
 
+    /**
+     * Instantiates a new light.
+     *
+     * @param period the period
+     * @param green the green
+     */
     public Light(int period, int green) {
 	this.period = period;
 	this.green = green;
     }
 
 
+    /**
+     * Gets the time.
+     *
+     * @return the time
+     */
     public int getTime() {
     	return time;
     }
 
+    /**
+     * Step.
+     */
     public void step() {
     	if(this.time < this.period - 1){
     		this.time++;
@@ -24,6 +48,11 @@ public class Light {
     	// Stegar fram klocka ett steg
     }
 
+    /**
+     * Checks if is green.
+     *
+     * @return true, if is green
+     */
     public boolean isGreen()   {
 	return (this.time < this.green);
 	// Returnerar true om time<green, annars false
@@ -31,6 +60,9 @@ public class Light {
 
     
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     public String toString() {
 	return "Light(period=" + this.period + ", time=" + this.time + ", green=" + this.green + ")";
     }
