9eebd83ef34050fd4a01350bf9f901b9b37aa1eb
Snapple49 <oliver-dot-stein@hotmail.com>
2014-11-28 12:42:30 +0100

    Ändrat i properties, lagt till en test suite

---
+++ TrafficSimTestSuite.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	LaneTest.class,
+	CarTest.class,
+	LightTest.class,
+	TrafficSystemTest.class
+})
+
+public class TrafficSimTestSuite {
+
+	
+
+}
